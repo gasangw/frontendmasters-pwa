89785efbf73377cbaa7997e99723f859c6afc10b
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-03-12 07:01:23 +0200

    refactor: add more assets to the assets array

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,5 @@
-const assets = ['styles.css', 'app.js', 'sw-register.js']
+const assets = ['/','styles.css', 'app.js', 'sw-register.js', 
+'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2']
 
 self.addEventListener("install", event => {
   caches.open('assets').then(cache => {
