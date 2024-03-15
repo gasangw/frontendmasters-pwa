d72b142086b8db76ad6a192dd3a61b9884172e1c
Thomas Gasangwa <thomasgasangwa@gmail.com>
2024-03-15 11:37:34 +0200

    feat: add a new icon with 512x512

--- serviceworker.js
+++ serviceworker.js
@@ -2,7 +2,20 @@ const assets = ['/','styles.css', 'app.js', 'sw-register.js',
 'https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2']
 
 self.addEventListener("install", event => {
-  caches.open('assets').then(cache => {
-    cache.addAll(assets)
-  })
+  event.waitUntil(
+    caches.open('assets').then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request).then(cachedResponse => {
+        return cachedResponse || fetch(event.request)
+      }).catch(error => {
+        console.error('Error in fetch handler:', error);
+        throw error;
+      })
+    )
 })
